ed89eb893a40450d344c3e4adc1be0be50df39e6
TahminaChowdhury <tahmina10st@gmail.com>
2021-10-20 19:24:22 +0600

    commit

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -3,7 +3,7 @@ import './Home.css'
 import { Container, Row } from 'react-bootstrap';
 import Banner from '../Banner/Banner';
 import Services from '../Services/Services';
-import ContactUs from '../ContactUs/ContactUs'
+import img from '../../images/successful-medical-team_329181-9252.jpg'
 import img4 from '../../images/mental-health-1.jpg'
 import img5 from '../../images/hand-drawn-illustrations-people-with-mental-health-problems_23-2149048909.jpg'
 
@@ -50,7 +50,42 @@ const Home = () => {
                     </div>
                 </div>
             </div>
-            <ContactUs></ContactUs>
+            <div className="row bg mt-5 pt-5 text-white">
+                <div className="col-sm-12 col-md-6 col-md-6">
+                    <div className="p-5">
+                    <h1>Contact Info</h1>
+                    <hr className="hr" />
+                    <h5>Need to discuss before treatment?</h5>
+                    <div className="d-flex mt-5">
+                        <div className="call-icon">
+                            <span className="pe-4 fs-3"><i class="fas fa-phone-alt"></i></span>
+                        </div>
+                        <div className="ps-4 fs-5">
+                            <p>01755-555-80</p>
+                            <p>998877665544</p>
+                        </div>
+                    </div>
+                    <div className="d-flex mt-5">
+                        <div className="call-icon">
+                            <span className="pe-4 fs-3"><i class="fas fa-map-marker-alt"></i></span>
+                        </div>
+                        <div className="ps-4 fs-5">
+                            <p>Sector-1,Road-3/E <br /> 
+                            Uttara,Dhaka-1230
+                            </p>
+                        </div>
+                    </div>
+                    <p className="pt-4">The best contact you will get here MIND HEART CLINIC</p>
+                    </div>
+                </div>
+
+                <div className="col-sm-12 col-md-6 col-lg-6">
+                <div className="py-5">
+                    <img src={img} alt="" className="img-fluid"/>
+                </div>
+                </div>
+
+            </div>
         </div>
     );
 };
